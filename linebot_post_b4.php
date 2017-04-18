08a4126009405573495f423dd10b28e77e9550aa
Kotaro Terada <tera@toki.waseda.jp>
2017-04-18 14:41:10 +0900

    Create LINE bot (forgot to commit these for long days)

---
+++ linebot_post_b4.php
@@ -0,0 +1,131 @@
+<?php
+require_once(__DIR__ . 'linebot.config.php');
+require_once(__DIR__ . '/googleapi.config.php');
+require_once(__DIR__ . '/google-api-php-client/src/Google/autoload.php');
+
+define('APPLICATION_NAME', 'Google Calendar API PHP Quickstart');
+define('CREDENTIALS_PATH', '~/.credentials/calendar-php-quickstart.json');
+define('CLIENT_SECRET_PATH', __DIR__ . '/client_secret.json');
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/calendar-php-quickstart.json
+define('SCOPES', implode(' ', array(
+  Google_Service_Calendar::CALENDAR_READONLY)
+));
+
+if (php_sapi_name() != 'cli') {
+  throw new Exception('This application must be run on the command line.');
+}
+
+/**
+ * Returns an authorized API client.
+ * @return Google_Client the authorized client object
+ */
+function getClient() {
+  $client = new Google_Client();
+  $client->setApplicationName(APPLICATION_NAME);
+  $client->setScopes(SCOPES);
+  $client->setAuthConfigFile(CLIENT_SECRET_PATH);
+  $client->setAccessType('offline');
+
+  // Load previously authorized credentials from a file.
+  $credentialsPath = expandHomeDirectory(CREDENTIALS_PATH);
+  if (file_exists($credentialsPath)) {
+    $accessToken = file_get_contents($credentialsPath);
+  } else {
+    // Request authorization from the user.
+    $authUrl = $client->createAuthUrl();
+    printf("Open the following link in your browser:\n%s\n", $authUrl);
+    print 'Enter verification code: ';
+    $authCode = trim(fgets(STDIN));
+
+    // Exchange authorization code for an access token.
+    $accessToken = $client->authenticate($authCode);
+
+    // Store the credentials to disk.
+    if(!file_exists(dirname($credentialsPath))) {
+      mkdir(dirname($credentialsPath), 0700, true);
+    }
+    file_put_contents($credentialsPath, $accessToken);
+    printf("Credentials saved to %s\n", $credentialsPath);
+  }
+  $client->setAccessToken($accessToken);
+
+  // Refresh the token if it's expired.
+  if ($client->isAccessTokenExpired()) {
+    $client->refreshToken($client->getRefreshToken());
+    file_put_contents($credentialsPath, $client->getAccessToken());
+  }
+  return $client;
+}
+
+/**
+ * Expands the home directory alias '~' to the full path.
+ * @param string $path the path to expand.
+ * @return string the expanded path.
+ */
+function expandHomeDirectory($path) {
+  $homeDirectory = getenv('HOME');
+  if (empty($homeDirectory)) {
+    $homeDirectory = getenv("HOMEDRIVE") . getenv("HOMEPATH");
+  }
+  return str_replace('~', realpath($homeDirectory), $path);
+}
+
+// Get the API client and construct the service object.
+$client = getClient();
+$service = new Google_Service_Calendar($client);
+
+// Print the next 10 events on the user's calendar.
+$optParams = array(
+  'maxResults' => 10,
+  'orderBy' => 'startTime',
+  'singleEvents' => TRUE,
+  'timeMin' => date('c'),
+);
+$results = $service->events->listEvents(CALENDAR_ID, $optParams);
+
+$todaystr = date('Y-m-d');
+$nicchoku = '日直なし';
+if (count($results->getItems()) == 0) {
+    print "No upcoming events found.\n";
+} else {
+    print "Upcoming events:\n";
+    foreach ($results->getItems() as $event) {
+        $datestr = $event->start->date;
+        //$timestamp = strtotime($datestr);
+        //print date('Y/m/d H:i:s', $timestamp);
+        if ($datestr === $todaystr) {
+            $nicchoku = str_replace('日直　', '', $event->getSummary());
+        }
+        printf("%s (%s)\n", $event->getSummary(), $datestr);
+    }
+}
+
+// -------------------------------- //
+// LINE BOT 投稿処理
+
+$text = '[' . $todaystr . '] 日直: ' . $nicchoku;
+$text = urlencode($text);
+
+$url = "https://slack.com/api/chat.postMessage?token=" . SLACK_API_KEY
+     . "&channel=%23general&username=" . BOT_NAME . "&icon_emoji=" . BOT_ICON . "&text=${text}";
+$response = file_get_contents($url);
+print $response . "\n";
+
+function api_post_request($path, $post) {
+    $url = "https://trialbot-api.line.me{$path}";
+    $headers = array(
+        'Content-Type: application/json',
+        'X-Line-ChannelID: {' . LINEBOT_CHANNEL_ID . '}',
+        'X-Line-ChannelSecret: {' . LINEBOT_CHANNEL_SECRET . '}',
+        'X-Line-Trusted-User-With-ACL: {' . LINEBOT_MID . '}'
+    );
+
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $output = curl_exec($url);
+    error_log($output);
+}
