59ace03ed3cd13158edd9b22b0144ddc45b07b62
Kotaro Terada <tera@toki.waseda.jp>
2017-04-24 10:42:59 +0900

    Update weather functions

--- slack_post_weather.php
+++ slack_post_weather.php
@@ -6,63 +6,9 @@ if (php_sapi_name() != 'cli') {
   throw new Exception('This application must be run on the command line.');
 }
 
+
 $todaystr = date('Y-m-d');
-$postdata = array(
-    // 天気
-    'weather' => array(
-        // 午前 (9時)
-        array(
-            'main' => '不明',
-            'icon' => ':question:'
-        ),
-        // 午後 (15時)
-        array(
-            'main' => '不明',
-            'icon' => ':question:'
-        ),
-        // 夜 (21時)
-        array(
-            'main' => '不明',
-            'icon' => ':question:'
-        )
-    ),
-    // 気温
-    'temperature' => array(
-        'max' => -999,
-        'min' => 999
-    )
-);
-
-
-$url = 'http://api.openweathermap.org/data/2.5/forecast?q=' . OWM_CITY . '&units=metric&appid=' . OWM_API_KEY;
-$response = file_get_contents($url);
-$responsedata = json_decode($response, true);
-if ($responsedata['cod'] === '200') {
-    // `list` は3時間ごとに入っている
-    // 9時 -> 午前、15時 -> 午後、21時 -> 夜 ということにする
-    foreach ($responsedata['list'] as $slot) {
-        if ($slot['dt_txt'] === $todaystr . ' 09:00:00') {
-            $postdata['weather'][0]['main'] = $slot['weather'][0]['main'];
-            $info = get_weather_info($slot['weather'][0]['id']);
-            $postdata['weather'][0]['icon'] = $info['icon'];
-        } else if ($slot['dt_txt'] === $todaystr . ' 15:00:00') {
-            $postdata['weather'][1]['main'] = $slot['weather'][0]['main'];
-            $info = get_weather_info($slot['weather'][0]['id']);
-            $postdata['weather'][1]['icon'] = $info['icon'];
-        } else if ($slot['dt_txt'] === $todaystr . ' 21:00:00') {
-            $postdata['weather'][2]['main'] = $slot['weather'][0]['main'];
-            $info = get_weather_info($slot['weather'][0]['id']);
-            $postdata['weather'][2]['icon'] = $info['icon'];
-        }
-
-        // 最高気温/最低気温
-        if (substr($slot['dt_txt'], 0, 10) === $todaystr) {
-            //print $slot['main']['temp'] . "\n";
-            $postdata['temperature']['max'] = max((int)round($slot['main']['temp']), $postdata['temperature']['max']);
-            $postdata['temperature']['min'] = min((int)round($slot['main']['temp']), $postdata['temperature']['min']);
-        }
-    }
-}
+$postdata = get_postdata($todaystr);
 
 
 // -------------------------------- //
