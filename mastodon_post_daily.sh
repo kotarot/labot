ffcd2cac631b77b2b65e8fda864587c0baae7d58
Kotaro Terada <tera@toki.waseda.jp>
2017-04-26 18:59:15 +0900

    Update usual weather

--- mastodon_post_daily.sh
+++ mastodon_post_daily.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 echo $SCRIPT_DIR
 
-php ${SCRIPT_DIR}/mastodon_post_weather.php
+php ${SCRIPT_DIR}/mastodon_post_yahooweather.php
 php ${SCRIPT_DIR}/mastodon_post_zemi.php today
