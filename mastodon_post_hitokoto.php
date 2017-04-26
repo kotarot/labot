a372b1583576a0f483dad8065eddea08ed0091a3
Kotaro Terada <tera@toki.waseda.jp>
2017-04-27 01:20:52 +0900

    Update hitokoto text

--- mastodon_post_hitokoto.php
+++ mastodon_post_hitokoto.php
@@ -10,26 +10,23 @@ if (php_sapi_name() != 'cli') {
 
 $text_candidates = array(
     'coffee' =>
-        '10時になったよ! 研究を中断して休憩しよう :coffee: :coffee: :coffee: :coffee:',
+        '10時になったよ! 研究をやめにして休憩しよう :coffee: :coffee: :coffee: :coffee:',
     'lunch' =>
-        '12時になったよ! 研究を中断してご飯を食べよう :fries: :pizza: :hotdog: :taco: :burrito: :stuffed_flatbread: :curry: :ramen: :spaghetti: :sushi: :fried_shrimp:',
+        '12時になったよ! 研究をやめにしてご飯を食べよう :fries: :pizza: :hotdog: :taco: :burrito: :stuffed_flatbread: :curry: :ramen: :spaghetti: :sushi: :fried_shrimp:',
     'nap' =>
-        '2時になったよ! 研究を中断して昼寝をしよう :sleeping_accommodation: :sleeping_accommodation: :sleeping_accommodation: :sleeping_accommodation:',
+        '2時になったよ! 研究をやめにして昼寝をしよう :sleeping_accommodation: :sleeping_accommodation: :sleeping_accommodation: :sleeping_accommodation:',
     'oyatsu' =>
-        '3時になったよ! 研究を中断しておやつを食べよう :pancakes: :doughnut: :cookie: :dango: :chocolate_bar: :custard:',
+        '3時になったよ! 研究をやめにしておやつを食べよう :pancakes: :doughnut: :cookie: :dango: :chocolate_bar: :custard:',
     'beer' =>
-        '定時になったよ! 研究を中断してビールを飲もう :beer: :beers: :beer: :beers: :beer: :beers: :beer: :beers: :beer: :beers: :beer: :beers: :beer: :beers: :beer: :beers:',
+        '定時になったよ! 研究をやめにしてビールを飲もう :beer: :beers: :beer: :beers: :beer: :beers: :beer: :beers: :beer: :beers: :beer: :beers: :beer: :beers: :beer: :beers:',
     'gotobed' =>
-        '0時になったよ! 研究を中断しておやすみ :zzz: :zzz: :zzz: :zzz: :zzz: :zzz: :zzz: :zzz:'
+        '0時になったよ! 研究をやめにして今日はおやすみ :zzz: :zzz: :zzz: :zzz: :zzz: :zzz: :zzz: :zzz:'
 );
 
 $text = '';
 if (array_key_exists($argv[1], $text_candidates)) {
     $text = $text_candidates[$argv[1]];
-    $text = urlencode($text);
-}
-if ($text !== '') {
-    $command = 'curl -X POST -d "access_token=' . MASTODON_ACCESS_TOKEN . '&status=' . $text
+    $command = 'curl -X POST -d "access_token=' . MASTODON_ACCESS_TOKEN . '&status=' . urlencode($text)
              . '&visibility=public" -Ss https://' . MASTODON_HOST . '/api/v1/statuses';
     exec($command, $out, $ret);
     var_dump($out);
